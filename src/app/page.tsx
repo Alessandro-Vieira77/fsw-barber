cb8c07d701a1d3b3235b1b8842c6c7f751ce48b1
Alessandro-Vieira77 <alessandrovieiraa77@gmail.com>
2025-08-01 09:41:51 -0300

    chore: add chadsn setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import { Calendar } from "lucide-react"
+import { Button } from "./_components/ui/button"
 export default function Page() {
   return (
     <div>
       <h1 className="mb-5 bg-red-600 text-blue-500">Home page</h1>
+      <Button>Click me</Button>
+      <Calendar className="size-6 text-blue-500" />
     </div>
   )
 }
